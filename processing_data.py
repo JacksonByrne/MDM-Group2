2a63039d7fd2d85461e68e3f02e05eb5125c19f5
jamieklinger <jamieklinger123@gmail.com>
2026-02-26 17:13:31 +0000

    added functions that get data for a metric and a country. Plot a bar chart for number of NaN for each metric

--- processing_data.py
+++ processing_data.py
@@ -4,8 +4,35 @@ import pandas as pd
 import seaborn as sns
 # read data set
 data=pd.read_csv('WorldSustainabilityDataset.csv')
-# get rows of data set that belong to specific countries in array, that meet condition that the continent is South America
-data_test=data[data['Continent']=='South America']
-# plots the data for the given column list in the x_axis and the column list for the y_axis, hue creates a key for each country
-sns.lineplot(data_test, x='Year', y=data.iloc[:,29], hue='Country Name')
+# get list of countries
+list_of_countries=list(set(data['Country Name']))
+def country_metric(country, metric):
+    '''
+    Returns the column of metric for the specific country
+    :param country: name of country, string
+    :param metric: name of metric, string
+    '''
+    country_data=data[data['Country Name']==country]
+    return (country_data[['Year',metric]])
+def plot_country_metric(country, metric):
+    '''
+    Produces a line plot for a metric for a country over the years
+    
+    :param country: name of country, string
+    :param metric: name of metric, string
+    '''
+    sns.lineplot(country_metric(country, metric), x='Year',y=metric)  
+def metric_plot():
+    '''
+    For each metric plots each the number of NaN values they have as a bar chart
+    '''
+    figs, axes=plt.subplots(1,1)
+    axes.bar(x=range(0,len(data.isnull().sum())),height=data.isnull().sum())
+    axes.set_xlabel('Metric Index')
+    axes.set_ylabel('Count')
+    axes.set_title('Number of NaN for each metric')
+metric_plot()
+
+
+
 plt.show()
