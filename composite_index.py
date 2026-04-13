826784bcdb313df2c10c691f3f821d2a62cbe28c
jamieklinger <jamieklinger123@gmail.com>
2026-04-13 18:10:04 +0100

    Improved readability, function to plot goals for country

--- composite_index.py
+++ composite_index.py
@@ -2,60 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
-# read data set
-data=pd.read_csv('new_WorldSustainabilityDataset.csv')
-# get list of countries
-list_of_countries=list(set(data['Country Name']))
-def country_metric(country, metric,df):
-    '''
-    Returns the column of metric for the specific country
-    :param country: name of country, string
-    :param metric: name of metric, string
-    '''
-    country_data=df[df['Country Name']==country]
-    return (country_data[['Year',metric]])
-def plot_country_metric(country, metric,df, goal_labels):
-    '''
-    Produces a line plot for a metric for a country over the years
-    
-    :param country: name of country, string
-    :param metric: name of metric, string
-    '''
-    sns.lineplot(country_metric(country, metric,df), x='Year',y=metric, label=goal_labels[metric[-6:].replace(" ","")])  
-def metric_plot():
-    '''
-    For each metric plots each the number of NaN values they have as a bar chart
-    '''
-    figs, axes=plt.subplots(1,1)
-    axes.bar(x=range(0,len(data.isnull().sum())),height=data.isnull().sum())
-    axes.set_xlabel('Metric Index')
-    axes.set_ylabel('Count')
-    axes.set_title('Number of NaN for each metric')
 
-def norm_columns(df, data):
-    # Creates a new updated dataframe where the values have been normalised, i.e put the values between 0 and 1 bases on means and standard deviations
-    # add list of metrics here only ones with numerical values, that can be normalised
-    metrics=data.columns.values[3:]
-    # create array of indexs showing if a metric is good to be low or high
-    lower_values_good=[2,4,7,9,13,17,18,22,23,26,27,30,32,37,40,42,45,46,47,48,51,52,53,54,55,60,62,63,64,65,71,72]
-    years=np.linspace(2002,2023,23)
-    # loop through each metric
-    for i,metric in enumerate(metrics):
-        # get min and max values of metric
-        min=np.nanmin(df[metric])
-        max=np.nanmax(df[metric])
-        # loop through each year calculating normalised value for each country for metric and updating the dataframe
-        for year in years:
-            year_values=(df[df['Year']==int(year)][metric])
-            norm_score=(year_values-min)/(max-min)
-            # if lower value is better change the normilisation score
-            if i in lower_values_good:
-                norm_score=1-norm_score
-            # update dataframe with normalised value
-            new_df=pd.DataFrame({metric:norm_score})
-            df.update(new_df)
-# copy dataframe to keep imported dataframe
-df=data.copy()
 # list of goals
 goal_labels = {
     "Goal1":  "No Poverty",
@@ -179,58 +126,150 @@ goal_metrics_labels = {
     "Personal remittances, received (% of GDP)",
     "Net official development assistance and official aid received (current US$) [DT.ODA.ALLD.CD]"
     ],
-
 }
-norm_columns(df,data)
-# loop through each goal and caluclate a mean for all of the metrics to get a composite index for each country for each year for that goal
-goals=goal_metrics_labels.keys()
-goals_columns=[f'Composite Index {goal}' for goal in goals]
-for goal in goals:
-    metrics_for_goal=goal_metrics_labels[goal]
-    # creates a new mean which is the composite index of the two goals
-    # decide how to combine metrics? For now just used standard mean. Could use median, have weight for specific goals
-    # add column to dataframe for each composite index
-    df[f'Composite Index {goal}'] = df[metrics_for_goal].mean(axis=1)
-# different plots to just show what the data now looks like
-# plot each goal for a specific country
-df['Goal index']=df[goals_columns].mean(axis=1)
-print(df.head())
-'''
-country='United Kingdom'
-for goal in goals:
-    plot_country_metric(country, f'Composite Index {goal}',df, goal_labels)
-plt.ylabel('Composite Index')
-plt.title(f'Goals for {country} over time')
-plt.legend(loc='upper left')
-plt.xlim(2001,2024)
-plt.show()
-'''
-# get countries with biggest change in last 10 years
-df_start=df[df['Year']==2005]
-df_end=df[df['Year']==2022]
-# change in composite index between 2002 and 2023
-change=df_end['Goal index'].to_numpy()-df_start['Goal index'].to_numpy()
+def country_metric(country, metric,df):
+    '''
+    Returns the column of metric for the specific country
+    :param country: name of country, string
+    :param metric: name of metric, string
+    '''
+    country_data=df[df['Country Name']==country]
+    return (country_data[['Year',metric]])
 
-# get 10 smallest and 10 biggest changes
-max_indeces=np.argsort(change)[-10:]
-min_indeces=np.argsort(change)[:10]
-print(change[max_indeces])
-# find these countries and plot the composite index over time
-countries=np.array(list(dict.fromkeys(df['Country Name'].to_numpy())))
-biggest_change_countries=countries[max_indeces]
-smallest_change_countries=countries[min_indeces]
-for country in biggest_change_countries:
-    sns.lineplot(country_metric(country,'Goal index',df), x='Year',y='Goal index', label=country)
-    plt.ylim(0,1)
-    plt.title('Composite index over time')
-    plt.xlim(2001,2024)
-plt.show()
-for country in smallest_change_countries:
-    sns.lineplot(country_metric(country,'Goal index',df), x='Year',y='Goal index', label=country)
-    plt.ylim(0,1)
-    plt.title('Composite index over time')
-    plt.xlim(2001,2024)
-plt.show()
+def plot_country_metric(country, metric,df, goal_labels):
+    '''
+    Produces a line plot for a metric for a country over the years
+    
+    :param country: name of country, string
+    :param metric: name of metric, string
+    '''
+    sns.lineplot(country_metric(country, metric,df), x='Year',y=metric, label=goal_labels[metric[-6:].replace(" ","")])  
+
+def metric_plot():
+    '''
+    For each metric plots each the number of NaN values they have as a bar chart
+    '''
+    figs, axes=plt.subplots(1,1)
+    axes.bar(x=range(0,len(data.isnull().sum())),height=data.isnull().sum())
+    axes.set_xlabel('Metric Index')
+    axes.set_ylabel('Count')
+    axes.set_title('Number of NaN for each metric')
+
+def normalise_columns(df, data):
+    '''
+    Creates a new updated dataframe where the values have been normalised, i.e put the values between 0 and 1 bases on means and standard deviations
+
+    param df: copied dataframe, pandas dataframe
+    param data: original dataframe, pandas dataframe
+    '''
+    # add list of metrics here only ones with numerical values, that can be normalised
+    metrics=data.columns.values[3:]
+    # create array of indexs showing if a metric is good to be low or high
+    lower_values_good=[2,4,7,9,13,17,18,22,23,26,27,30,32,37,40,42,45,46,47,48,51,52,53,54,55,60,62,63,64,65,71,72]
+    years=np.linspace(2002,2023,23)
+    # loop through each metric
+    for i,metric in enumerate(metrics):
+        # get min and max values of metric
+        min=np.nanmin(df[metric])
+        max=np.nanmax(df[metric])
+        # loop through each year calculating normalised value for each country for metric and updating the dataframe
+        for year in years:
+            year_values=(df[df['Year']==int(year)][metric])
+            norm_score=(year_values-min)/(max-min)
+            # if lower value is better change the normilisation score
+            if i in lower_values_good:
+                norm_score=1-norm_score
+            # update dataframe with normalised value
+            new_df=pd.DataFrame({metric:norm_score})
+            df.update(new_df)
+
+def add_composite_indexes_to_dataframe(df):
+    '''
+    loop through each goal and caluclate a mean for all of the metrics to get a composite index for each country for each year for that goal
+
+    param df: dataframe
+    '''
+    goals=goal_metrics_labels.keys()
+    for goal in goals:
+        metrics_for_goal=goal_metrics_labels[goal]
+        # creates a new mean which is the composite index of the two goals
+        # decide how to combine metrics? For now just used standard mean. Could use median, have weight for specific goals
+        # add column to dataframe for each composite index
+        df[f'Composite Index {goal}'] = df[metrics_for_goal].mean(axis=1)
+    # then take the mean of all the goal composite indexes to get an overall composite index for each country each year
+    goals_columns=[f'Composite Index {goal}' for goal in goals]
+    df['Overall Composite Index']=df[goals_columns].mean(axis=1)
+
+def plot_composite_index_for_list_countries(df,countries,composite_index):
+    '''
+    plot a composite index column for different countries over time
+
+    param df: dataframe
+    param countries: list of countries, list
+    param composite_index: composite index column header, string
+    '''
+    for country in countries:
+        # ignore countries that have a low composite index value in 2002 due to lack of data 
+        countrydf=df[df['Country Name']==country]
+        countrydf=countrydf[countrydf['Year']==2002]
+        # only plot data between these years
+        df_filtered = df[(df['Year'] >= 2005) & (df['Year'] <= 2021)]
+        if list(countrydf[composite_index])[0]>0.3:
+            sns.lineplot(country_metric(country,composite_index,df_filtered), x='Year',y=composite_index, label=country)
+            plt.ylim(0.3,0.75)
+            plt.title('Overall Composite Index over time')
+            plt.xlim(2005,2022)
+    plt.show()
+
+def get_countries_with_biggest_changes(df):
+    '''
+    gets countries with the biggest positive and negatives changes in the overall composite index between 2005 and 2022
+
+    param df: dataframe
+    '''
+    # get countries with biggest change between 2005 and 2021, this because there is less data near the edges so values aren't as accurate
+    df_start=df[df['Year']==2005]
+    df_end=df[df['Year']==2021]
+    # change in composite index between 2005 and 2021
+    change=df_end['Overall Composite Index'].to_numpy()-df_start['Overall Composite Index'].to_numpy()
+    # get 10 smallest and 10 biggest changes
+    max_indeces=np.argsort(change)[-10:]
+    min_indeces=np.argsort(change)[:10]
+    # find these countries and plot the composite index over time
+    countries=np.array(list(dict.fromkeys(df['Country Name'].to_numpy())))
+    biggest_pos_change_countries=countries[max_indeces]
+    biggest_neg_change_countries=countries[min_indeces]
+    return (biggest_pos_change_countries,biggest_neg_change_countries)
+def plot_goals_for_country(df,country,goals):
+    '''
+    plot specific composite indexes of goals for a country over time
+
+    param df: dataframe
+    param country: name of country, string
+    param goals, list of goals, list
+    '''
+    # only plot data between these years
+    df_filtered = df[(df['Year'] >= 2005) & (df['Year'] <= 2021)]
+    # loop through plot each composite index of goal
+    for goal in goals:
+        plot_country_metric(country, f'Composite Index {goal}',df_filtered, goal_labels)
+    plt.ylabel('Composite Index')
+    plt.title(f'Goals for {country} over time')
+    plt.legend(loc='upper left')
+    plt.xlim(2005,2022)
+    plt.show()
+# read data set
+data=pd.read_csv('new_WorldSustainabilityDataset.csv')
+# copy dataframe to keep imported dataframe
+df=data.copy()
+normalise_columns(df,data)
+add_composite_indexes_to_dataframe(df)
+big_pos_change,big_neg_change=get_countries_with_biggest_changes(df)
+plot_composite_index_for_list_countries(df, big_pos_change, 'Overall Composite Index')
+plot_composite_index_for_list_countries(df, big_neg_change, 'Overall Composite Index')
+goals=['Goal10','Goal11','Goal13','Goal15','Goal16','Goal17']
+plot_goals_for_country(df,'Bolivia',goals)
+
 
 
 def country_goal_data(country, goal, df):
